ad608ed6a1ac30e787f6f1830fed705c2b29fe50
Fadia04 <fl.medja@gmail.com>
2022-09-12 16:08:06 +0200

    Deployment V4

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 # STATICFILES_DIRS = [BASE_DIR.joinpath('static'),]
 
-#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_ROOT = "/media/"
 
