a1d8011b3a8ae148aab4fb46a9e0411459f5c72f
Fadia04 <fl.medja@gmail.com>
2022-10-18 07:35:50 +0200

    settings

--- purbeurre/settings/__init__.py
+++ purbeurre/settings/__init__.py
@@ -142,9 +142,9 @@ INTERNAL_IPS = ['127.0.0.1']
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-# MEDIA_ROOT = "/media/"
+MEDIA_ROOT = "/media/"
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
